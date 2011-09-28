4f9e477b5a7c1ede385c65d23a255bfc4419789c
Miro Varilo <miro.varilo@gmail.com>
2011-09-29 00:35:52 +0300

    test

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Kirjautuminen</title>
+    </head>
+    <body>
+        <form action="login.php" method="post">
+            <p>Nimi: <br>
+                <input type="text" name="tunnus">
+            </p>
+            <p>Salasana: <br>
+                <input type="password" name="salasana">
+            </p>
+            <input type="submit" value="Kirjaudu">
+        </form>
+    </body>
+</html>
